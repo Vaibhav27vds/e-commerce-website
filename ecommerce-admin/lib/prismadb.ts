473c1597da1ad4b90617ccc4048c66a195fb0e2e
Vaibhav Deep Srivastava <128883944+Vaibhav27vds@users.noreply.github.com>
2024-01-18 03:58:38 +0000

    prisma connected with mongo db atlas

--- ecommerce-admin/lib/prismadb.ts
+++ ecommerce-admin/lib/prismadb.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-    var prisma: PrismaClient
+    var prisma: PrismaClient | undefined
 };
 
-const prismadb = globalThis.prisma
+const prismadb = globalThis.prisma || new PrismaClient();
 if(process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
 
 export default prismadb;
+ 
